6ec7135b61ffd886014186f5cd2a5ed93ac30e97
BartaFerenc <benkobalint04@gmail.com>
2022-05-26 20:57:34 +0200

    a

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ but = document.getElementById('but');
 krater = document.getElementById('krater');
 ip = document.getElementById('ip');
 
-//when but pressed change translated text to "asd"
 but.addEventListener('click', function(){
     if (mainbox.value.length > 0){
         translated.innerHTML = "Fordítás.....";
         //wait 10 seconds
         setTimeout(function(){
             translated.innerHTML = "Kráter";
-            krater.style.display = "block";
+            
+            
+            //change the 'translated' background image to 'kukac.png'
+            translated.style.backgroundImage = "url('kukac.png')";
+            krater.addEventListener('click', function(){
+                document.body.style.backgroundColor = "green";
+            }
+            );
         }, 10000);
     }
     else{
