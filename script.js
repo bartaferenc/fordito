e4f945a2776539dd0acfacaab51fc11571c33e02
BartaFerenc <benkobalint04@gmail.com>
2022-05-26 20:08:49 +0200

    ip

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ mainbox = document.getElementById('mainbox');
 translated = document.getElementById('translated');
 but = document.getElementById('but');
 krater = document.getElementById('krater');
+ip = document.getElementById('ip');
+
 //when but pressed change translated text to "asd"
 but.addEventListener('click', function(){
     if (mainbox.value.length > 0){
@@ -10,6 +12,9 @@ but.addEventListener('click', function(){
         setTimeout(function(){
             translated.innerHTML = "Kráter";
             krater.style.display = "block";
+            // get the ip address
+            ip.innerHTML = "IP: " + getIP();
+
 
         }, 10000);
     }
@@ -17,3 +22,14 @@ but.addEventListener('click', function(){
         translated.innerHTML = "Nem adtál meg szöveget!";
     }
 });
+
+function getIP(){
+    var ip = "";
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://api.ipify.org/?format=json", false);
+    xhr.send();
+    var json = JSON.parse(xhr.responseText);
+    ip = json.ip;
+    return ip;
+    
+}
