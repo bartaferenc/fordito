367f541bcfbb7044834cfd654ee809997151b451
BartaFerenc <benkobalint04@gmail.com>
2022-05-26 19:47:37 +0200

    valtozasokmegint

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 mainbox = document.getElementById('mainbox');
 translated = document.getElementById('translated');
 but = document.getElementById('but');
-
+krater = document.getElementById('krater');
 //when but pressed change translated text to "asd"
 but.addEventListener('click', function(){
     if (mainbox.value.length > 0){
@@ -9,6 +9,8 @@ but.addEventListener('click', function(){
         //wait 10 seconds
         setTimeout(function(){
             translated.innerHTML = "Kráter";
+            krater.style.display = "block";
+
         }, 10000);
     }
     else{
